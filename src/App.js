45159169a216273f4219aac906b663b3757c46b9
Kamlesh Prajapati <kamleshprajapati@Kamleshs-MacBook-Air.local>
2022-01-17 12:04:38 -0500

    all my new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import axios from "axios";
+import  { useEffect, useState} from "react"
 import './App.css';
 
 function App() {
+  const apiKey= 'sQc1luCX';
+
+  const [art, setArt] = useState([]);
+  const [userInput,setUserInput] = useState("");
+  const [searchTerm,setSearchTerm] = useState("");
+
+  // make an api call for rijik museum
+  useEffect( () => {
+    axios({
+      url: 'https://www.rijksmuseum.nl/api/en/collection',
+      method: "GET",
+      dataResponse: 'json',
+      params: {
+        key:apiKey,
+        q: searchTerm,
+        imgonly :true,
+      }
+    }).then( (response) => {
+      console.log(response.data.artObjects);
+      setArt(response.data.artObjects);
+    })
+  }, [searchTerm])
+
+ const handleInput = (e)=> {
+  setUserInput(e.target.value);
+ }
+ 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setSearchTerm(userInput);
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form action="#" onSubmit={handleFormSubmit}>
+        <label htmlFor="search" >Search</label>
+        <input type="input" id="search" onChange={handleInput} value={userInput} />
+        <button>Search</button>
+      </form>
+
+      {
+        art.map( (artWork) => {
+          return(
+            <div key={artWork.id}>
+              <h2>{artWork.longTitle}</h2>
+              <img src={artWork.webImage.url} alt="" />
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
 
 export default App;
+// sQc1luCX
+
+
+// Delete package-lock. json
+// Add mini-css- to dependencies bit of the package.json
+// "mini-css-extract-plugin": "2.4.7"
+// 3. navigate to your react app folder and run
+// npm install
+
+
+// npm run build ---- for deploying website
